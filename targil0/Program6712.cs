787f493dd769e07b73ab0038b6b6ed789f05c40b
Pxor2903 <armoun26@gmail.com>
2022-11-06 20:03:51 +0100

    Initial code +welcome fonctions

---
+++ targil0/Program6712.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace targil0 
+{
+    partial class Program 
+    {
+        static void Main(string[] args)
+        {
+            Welcome6712();
+            Welcome5260();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome5260();
+        private static void Welcome6712()
+        {
+            Console.Write("Enter your name: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console application", username);
+        }
+    }
+}
