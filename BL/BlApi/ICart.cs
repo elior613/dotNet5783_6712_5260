8c99a1feacac7ededf3c37ba5c165b7d27a5bdc9
Aaron Mimoun <armoun26@gmail.com>
2023-01-09 17:41:04 +0200

    continuation

--- BL/BlApi/ICart.cs
+++ BL/BlApi/ICart.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,29 @@ using System.Threading.Tasks;
 
 namespace BlApi
 {
+    /// <summary>
+    /// Adding a new product in cart (for catalog screen, product details screen)
+    /// </summary>
+    /// <param name="Cart"></param>
+    /// <param name="ProductID"></param>
+    /// <returns></returns>
     public interface ICart
     {
-        int Add(ICart cart);
-        void Update(ICart cart);
+        public Cart? Add(ICart cart, int ProductId);
+
+        /// <summary>
+        /// Updating the quantity of a product in the cart
+        /// </summary>
+        /// <param name="cart"></param>
+        public void Update(ICart cart);
+
+        ///<summary>
+        ///Confirmation of placing order in the cart
+        ///</summary>
+        ///<param name="Cart"></param>
+        ///
+        public void Confirmation(Cart cart);
+
+
     }
 }
